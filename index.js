90e312b96a1424e57f96a4c343b194d88f15a142
Dane Petersen <thegreatsunra@gmail.com>
2017-12-12 16:09:26 -0600

    Remove local download function now that's it's in a module

--- index.js
+++ index.js
@@ -28,18 +28,6 @@ async function createAndAssignCerts () {
     console.log('error', err)
   }
   }
-
-const download = (url, dest, cb) => {
-  const file = fs.createWriteStream(dest)
-  const request = https.get(url, (response) => { // eslint-disable-line no-unused-vars
-    response.pipe(file)
-    file.on('finish', () => {
-      file.close(cb) // close() is async, call cb after close completes.
-    })
-  }).on('error', (err) => { // Handle errors
-    fs.unlink(dest) // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message)
-  })
 }
 
 createAndAssignCerts()
