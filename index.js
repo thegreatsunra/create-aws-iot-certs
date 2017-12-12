59b1661d30a8e8ee4f19ea5b5594c5fa8ee81245
Dane Petersen <thegreatsunra@gmail.com>
2017-12-12 09:40:09 -0600

    Add generic utility for downloading URL and piping it to a file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const aws = require('aws-sdk')
+const https = require('https')
+const fs = require('fs')
 aws.config.update({ region: 'us-west-2' })
 const iot = new aws.Iot()
 const params = {
@@ -12,3 +14,17 @@ iot.createKeysAndCertificate(params, (err, data) => {
     console.log(data)
   }
 })
+
+const download = (url, dest, cb) => {
+  const file = fs.createWriteStream(dest)
+  const request = https.get(url, (response) => { // eslint-disable-line no-unused-vars
+    response.pipe(file)
+    file.on('finish', () => {
+      file.close(cb) // close() is async, call cb after close completes.
+    })
+  }).on('error', (err) => { // Handle errors
+    fs.unlink(dest) // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message)
+  })
+}
+
